49f4e07c48ef0cf50029c385e9a240bae743373b
u1_mas <mtakuya1022@gmail.com>
2025-01-29 00:17:21 +0900

    check type when push

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
+import { logger } from "./logger.ts";
+
 export function add(a: number, b: number): number {
   return a + b;
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  logger.debug("Add 2 + 3 =", add(2, 3));
 }
